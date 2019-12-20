55c591cd84325e6c39c3dab08eb836949c56b250
Michał <willy.eksel@gmail.com>
2019-12-20 09:40:06 +0100

    Validations added

--- src/components/Home/HomeContact.js
+++ src/components/Home/HomeContact.js
@@ -8,8 +8,7 @@ class HomeContact extends Component {
     errorNameMessage: "",
     errorEmailMessage: "",
     errorAreaMessage: "",
-    successMessage: "",
-    messageColor: "red"
+    successMessage: ""
   };
 
   validateEmail = email => {
@@ -24,96 +23,69 @@ class HomeContact extends Component {
   };
 
   handleSubmit = event => {
-    event.preventDefault();
+    // event.preventDefault();
+
+    this.setState({
+      errorNameMessage: "",
+      errorEmailMessage: "",
+      errorAreaMessage: "",
+      successMessage: ""
+    });
 
     const dataToSend = {
       name: this.state.name,
       email: this.state.email,
       message: this.state.message
     };
-
     console.log(dataToSend);
 
-    const url = "https://fer-api.coderslab.pl/v1/portfolio/contact";
+    let isNameOk = true;
+    let isEmailOk = true;
+    let isMessageOk = true;
 
-    return fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(dataToSend)
-    })
-      .then(response => {
-        if (response.ok) {
-          return response.json();
-        } else {
-          throw new Error("Blad sieci");
-        }
-      })
-      .catch(error => {
-        console.log(error);
+    if (dataToSend.name.indexOf(" ") !== -1) {
+      this.setState(state => ({
+        errorNameMessage: "Podane imię jest nieprawidłowe!"
+      }));
+      isNameOk = false;
+    }
+    if (!this.validateEmail(dataToSend.email)) {
+      this.setState(state => ({
+        errorEmailMessage: "Podany email jest nieprawidłowy"
+      }));
+      isEmailOk = false;
+    }
+    if (dataToSend.message.length < 120) {
+      this.setState(state => ({
+        errorAreaMessage: "Pole wiadomość musi mieć conajmniej 120 znaków!"
+      }));
+      isMessageOk = false;
+    }
+    if (isNameOk && isEmailOk && isMessageOk) {
+      this.setState({
+        successMessage: "Wiadomość została wysłana! Wkrótce się skontaktujemy."
       });
 
-    // this.setState({ errorMessage: "" });
+      const url = "https://fer-api.coderslab.pl/v1/portfolio/contact";
 
-    // let isOk = true;
-
-    // if (this.state.name.length.indexOf(" ") == -1) {
-    //   this.setState(state => ({
-    //     errorMessage: state.errorNameMessage + "Podane imię jest nieprawidłowe!"
-    //   }));
-    //   isOk = false;
-    // }
-    // if (!validateEmail(this.state.email)) {
-    //   this.setState(state => ({
-    //     errorMessage:
-    //       state.errorEmailMessage + "Podany email jest nieprawidłowy"
-    //   }));
-    //   isOk = false;
-    // }
-    // if (this.state.message.length < 120) {
-    //   this.setState(state => ({
-    //     errorMessage:
-    //       state.errorAreaMessage +
-    //       "Pole wiadomość musi mieć conajmniej 120 znaków!"
-    //   }));
-    //   isOk = false;
-    // }
-    // if (isOk) {
-    //   this.setState({
-    //     errorMessage: "Wiadomość została wysłana! Wkrótce się skontaktujemy.",
-    //     messageColor: "green"
-    //   });
-    //   return true;
-    // }
-
-    // const dataToSend = {
-    //   name: this.state.name,
-    //   email: this.state.email,
-    //   message: this.state.message
-    // };
-
-    // const addItem = data => {
-    //   const url = "https://fer-api.coderslab.pl/v1/portfolio/contact";
-
-    //   return fetch(url, {
-    //     method: "POST",
-    //     headers: {
-    //       "Content-Type": "application/json"
-    //     },
-    //     body: JSON.stringify(dataToSend)
-    //   }).then(response => {
-    //     if (response.ok) {
-    //       return response.json();
-    //     } else {
-    //       throw new Error("Blad sieci");
-    //     }
-    //   });
-    // };
-
-    // addItem(dataToSend).catch(error => {
-    //   console.log(error);
-    // });
+      return fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(dataToSend)
+      })
+        .then(response => {
+          if (response.ok) {
+            return response.json();
+          } else {
+            throw new Error("Blad sieci");
+          }
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }
   };
 
   render() {
@@ -122,9 +94,10 @@ class HomeContact extends Component {
         <div className="wrapper">
           <h2 className="header">Skontaktuj się z nami</h2>
           <div className="decoration"></div>
+          <p style={{ color: "green" }}>{this.state.successMessage}</p>
           <form className="contact-form" onSubmit={this.handleSubmit}>
             <div className="contact-form__heading">
-              <div className="contact-form__item">
+              <div className="contact-form__item contact-form__name">
                 <label>Wpisz swoje imię</label>
                 <input
                   type="text"
@@ -133,8 +106,9 @@ class HomeContact extends Component {
                   value={this.state.name}
                   onChange={this.handleChange}
                 />
+                <p style={{ color: "red" }}>{this.state.errorNameMessage}</p>
               </div>
-              <div className="contact-form__item">
+              <div className="contact-form__item contact-form__email">
                 <label>Wpisz swój email</label>
                 <input
                   type="email"
@@ -143,6 +117,7 @@ class HomeContact extends Component {
                   value={this.state.email}
                   onChange={this.handleChange}
                 />
+                <p style={{ color: "red" }}>{this.state.errorEmailMessage}</p>
               </div>
             </div>
 
@@ -154,13 +129,16 @@ class HomeContact extends Component {
                 onChange={this.handleChange}
                 placeholder="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
               />
+              <p style={{ color: "red" }}>{this.state.errorAreaMessage}</p>
             </div>
 
             <button
               className="contact-form__button"
               type="submit"
               value="Wyślij"
-            />
+            >
+              Wyślij
+            </button>
           </form>
         </div>
       </div>
